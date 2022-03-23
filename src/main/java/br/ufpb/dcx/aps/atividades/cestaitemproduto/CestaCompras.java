32235a25acc94a35f2c8095d29deffa6a10d4ef1
Rodrigo Reboucas <rodrigor@labs.dcx>
2022-03-23 12:02:11 -0300

    aula 23 março

---
+++ src/main/java/br/ufpb/dcx/aps/atividades/cestaitemproduto/CestaCompras.java
@@ -0,0 +1,35 @@
+package cestaitemproduto;
+
+import java.util.*;
+
+public class CestaCompras {
+
+    private Map<Produto,ItemCesta> mapItens = new LinkedHashMap<>();
+
+
+
+    public void addProduto(Produto produto){
+        mapItens.put(produto,new ItemCesta(produto));
+    }
+
+    public void addProduto(Produto produto, int quantidade){
+        mapItens.put(produto,new ItemCesta(produto,quantidade));
+
+    }
+
+
+    public void setQuantidade(Produto produto, int quantidade){
+        mapItens.get(produto).setQuantidade(quantidade);
+    }
+
+    public void removerProduto(Produto produto){
+        mapItens.remove(produto);
+
+    }
+
+    public List<ItemCesta> getItens(){
+        mapItens.values();
+    }
+
+
+}
---
+++ src/main/java/br/ufpb/dcx/aps/atividades/cestaitemproduto/ItemCesta.java
@@ -0,0 +1,35 @@
+package cestaitemproduto;
+
+public class ItemCesta {
+
+
+    private Produto produto;
+    private int quantidade;
+
+    public ItemCesta(Produto produto, int quantidade){
+        this.produto = produto;
+        this.quantidade = quantidade;
+    }
+
+    public ItemCesta(Produto produto){
+        this(produto, 1);
+    }
+
+    public Produto getProduto(){
+        return this.produto;
+    }
+
+    public int getQuantidade(){
+        return this.quantidade;
+    }
+
+    public void setQuantidade(int quantidade){
+        this.quantidade = quantidade;
+    }
+
+    public double getTotal(){
+        return this.produto.getPreco() * this.quantidade;
+    }
+
+
+}
